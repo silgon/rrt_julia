0f5465a8946d9f80484e5902bb7e194b24aea8e7
silgon <silgon3200@gmail.com>
2016-09-25 17:32:57 +0200

    other dynamics added to one of the files

--- rrtstar_3D.jl
+++ rrtstar_3D.jl
@@ -2,10 +2,12 @@ start = Float64[3, 3, π]
 goal = Float64[9, 9, 0]
 # start = Float64[3, 3, π/4]
 # goal = Float64[8, 2, -π/4]
-ε = 0.4 # meters
-α = pi/16
+# ε = 0.4 #
+ε_d_max = 0.4
+ε_d_min = 0.1
+ε_α = pi/12
 k_α = 1
-n_iter = 10000
+n_iter = 20000
 using Graphs
 
 g = graph(Int[], Graphs.IEdge[], is_directed=true)
@@ -26,17 +28,25 @@ function is_allowed(state::Array{Float64, 1})
     return true
 end
 
+function boundedAngle(angle)  #[-pi,pi]
+    atan2(sin(angle),cos(angle))
+end
+
 function dist(p1::Array{Float64,1}, p2::Array{Float64,1})
-    sqrt((p1[1]-p2[1])^2+(p1[2]-p2[2])^2+k_α^2*((p1[3]-p2[3]+pi)%pi-pi)^2)
+    sqrt((p1[1]-p2[1])^2+(p1[2]-p2[2])^2+(k_α*boundedAngle(p1[3]-p2[3]))^2)
 end
 
 function transition(p1::Array{Float64,1}, p2::Array{Float64,1})
-    if (dist(p1, p2) < ε) & (((p1[3]-p2[3]+pi)%pi-pi) < α)
-        return p2
-    else
-        return Float64[p1[1] + ε*cos(p1[3]+randn()*.2),
-        p1[2] + ε*sin(p1[3]+randn()*.2), p1[3]+randn()*.2]
-    end
+    d = min(ε_d_max, sqrt((p1[1]-p2[1])^2+(p1[2]-p2[2])^2))
+    d = max(ε_d_min, d)
+    p_12_θ = atan2(p2[2]-p1[2], p2[1]-p1[1])-p1[3]
+    α = min(ε_α, abs(p_12_θ))
+    s_α = sign(p_12_θ)
+    θ = min(ε_α, abs(p2[3]-p1[3]))
+    s_θ = sign(p2[3]-p1[3])
+    Float64[p1[1]+d*cos(p1[3]+s_α*α),
+            p1[2]+d*sin(p1[3]+s_α*α),
+            p1[3]+s_θ*θ]
 end
 
 # RRT algorithm
